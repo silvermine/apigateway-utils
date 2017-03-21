953dcc7824444116754b6c1dcaa6468e597c70e0
Jeremy Thomerson <jeremy@thomersonfamily.com>
2017-03-21 13:03:30 -0400

    Do not allow console statements except in specific locations

--- src/responseBuilderHandler.js
+++ src/responseBuilderHandler.js
@@ -5,12 +5,14 @@ var ResponseBuilder = require('./ResponseBuilder');
 module.exports = function(promiseReturningHandlerFn, request, cb) {
    promiseReturningHandlerFn()
       .then(function(respBuilder) {
+         // eslint-disable-next-line no-console
          console.log('completed with %s millis left', request.getContext().getRemainingTimeInMillis());
          cb(undefined, respBuilder.toResponse(request));
       })
       .catch(function(err) {
          var respBuilder = new ResponseBuilder(request).error();
 
+         // eslint-disable-next-line no-console
          console.log('ERROR:', err, err.stack);
          cb(undefined, respBuilder.toResponse(request));
       })
